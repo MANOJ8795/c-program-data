b6ec1adc07103624601ce9ee16cc144ba7b31865
MANOJ8795 <manojmanu3512@gmail.com>
2024-09-14 09:58:50 +0530

    Add files via upload

---
+++ name program.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+void main()
+{
+  int n;
+  char str[10];
+  printf("Enter your name: ");
+  scanf("%s", str);
+  n=strlen(str);
+  printf("Hello %s, welcome to C programming.", str);
+}
+
+
+
+
