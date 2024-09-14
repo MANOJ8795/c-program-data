5741579c22f40e2a91ccbeb5d5582cddbf9727ff
MANOJ8795 <manojmanu3512@gmail.com>
2024-09-14 10:01:46 +0530

    Add files via upload

---
+++ string memory .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char str[100], str1[100];
+  scanf("%s", str);
+  scanf("%s", str1);
+  strcat(str, str1);
+    printf("%s", str);
+    }
+
+
+
+
+
